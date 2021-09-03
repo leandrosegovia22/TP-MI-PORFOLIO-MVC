677728ff70cc30661772ed253951202a7a3ffa9b
leandrosegovia22 <leandrosegoviaroman@gmail.com>
2021-09-03 03:05:22 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mainRouter = require('./routers/main')
+
+app.use(express.static('public'));
+
+app.use('/', mainRouter)
+
+
+app.listen(port, ()=> console.log('Servidor funcionando en el puerto ' + port));
